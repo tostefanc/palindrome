7034760f930b47e132870c196e8e51faa3d91b3a
tostefanc <stefan.toma100@gmail.com>
2021-02-24 20:23:17 +0200

    final

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-//Get the string (should use DOM)
-// let word = 'This is a string';
-// let word1 = 'madam';
-//Reverse a string without converting to array
 
 
 let button = document.getElementById('button');
 let clean = document.getElementById('clean');
-// let formInput = document.getElementById('input').value;
 let inputReversed = document.getElementById('input-reversed');
 let reversed = '';
 
@@ -28,47 +23,6 @@ function cleanEverything(){
 button.addEventListener('click', function(){
     let formInput = document.getElementById('input').value;
     wordReversed(formInput);
-//    check();
 });
 
 
-
-
-
-// let input = '';
-
-// function submitForm(event){
-//     event.preventDefault();
-//     let input = event.target['input'].value;
-//     let reversed = '';
-//     for (let i = input.length - 1; i >= 0; i --){
-//         reversed += input[i];
-//     }
-//     if(input === reversed){
-//         input.value=reversed;
-//     } else {
-//         // console.log('No');
-//         input.value='reversed';
-//     }
-// }
-
-
-
-
-
-// // console.log(input);
-
-// // if(){
-
-// // }
-
-
-// function check (string, reversed){
-//     if (reversed !== string){
-//         console.log('Not a palindrome');
-//     } else {
-//         console.log('Palindrome found');
-//     }
-// };
-
-// // check(word1, wordReversed);
